5d16907f33bd4729c09f7e0392e322496f38bf07
Rosa-Zambrana <ros.love.may0@gmail.com>
2017-11-09 19:47:52 -0400

    boton flotante animado en menu

--- app/src/main/java/notanamelessentreprise/kryptonote/Menu.java
+++ app/src/main/java/notanamelessentreprise/kryptonote/Menu.java
@@ -1,11 +1,16 @@
 package notanamelessentreprise.kryptonote;
 
 import android.os.Bundle;
+import android.support.design.internal.NavigationMenu;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import io.github.yavski.fabspeeddial.FabSpeedDial;
 
 public class Menu extends AppCompatActivity {
 
@@ -16,12 +21,32 @@ public class Menu extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });*/
+
+        FabSpeedDial fabSpeedDial =(FabSpeedDial)findViewById(R.id.fabSpeedDial);
+        fabSpeedDial.setMenuListener(new FabSpeedDial.MenuListener() {
+            @Override
+            public boolean onPrepareMenu(NavigationMenu navigationMenu) {
+                return true; //false don't show menu
+            }
+
+            @Override
+            public boolean onMenuItemSelected(MenuItem menuItem) {
+                Toast.makeText(Menu.this, "" + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public void onMenuClosed() {
+
             }
         });
     }
