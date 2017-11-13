9deb18bcd70169f046fd1057817e25f06aeb20b2
EValeriaAliagaTaborga <valealiaga2121@gmail.com>
2017-11-13 19:41:55 -0400

    Pattern lock view 2

--- app/src/main/java/notanamelessentreprise/kryptonote/MainActivity.java
+++ app/src/main/java/notanamelessentreprise/kryptonote/MainActivity.java
@@ -4,24 +4,28 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView layoutMenu;
+    private ImageView layoutMenu;
 
     private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         context = this;
 //        setContentView(R.layout.activity_edit_nota);
         //Intent intent = new Intent(context, EditNota.class);
         //startActivity(intent);
 
-        layoutMenu=(TextView) findViewById(R.id.layoutMenu);
+        layoutMenu=(ImageView) findViewById(R.id.layoutMenu);
 
         layoutMenu.setOnClickListener(new View.OnClickListener() {
             @Override
